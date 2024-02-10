13504604eb26c4f640b19b2d71a7bd6f9e522b21
miles <miles.munro2004@gmail.com>
2024-02-11 09:58:57 +1100

    Crawl Page working

--- crawl.js
+++ crawl.js
@@ -12,20 +12,61 @@ function getURLSFromHTML(htmlBody, baseURL){
     const atagArray = dom.window.document.querySelectorAll('a')
     const httpArray = []
     
-    atagArray.forEach(atagArray => {
-        const href = atagArray.href
-        if (href.startsWith('http')){
-            httpArray.push(href)
-        }
-        else if (href.startsWith('/')){
-            httpArray.push(baseURL+href)
+    atagArray.forEach(atag => {
+        try{
+            const href = atag.href
+            if (href.startsWith('http')){
+                httpArray.push(href)
+            }
+            else if (href.startsWith('/')){
+                const absoluteURL = new URL(href, baseURL).href
+                httpArray.push(absoluteURL)
+            }
+        } catch (err){
+            console.log(`${err.message}: ${atag.href}`)
         }
 
     })
     return httpArray
 }
 
+async function crawlPage(rootURL, currentURL, pages = {}) {
+    // Ensure currentURL is absolute
+    const baseHostName = new URL(baseURL).hostname;
+    const currentHostName = new URL(currentURL).hostname;
+    if (baseHostName !== currentHostName) {
+        return pages;
+    }
+    
+    const normalized = normalizeURL(currentURL);
+    if (normalized in pages) {
+        pages[normalized] += 1;
+        return pages;
+    }
+    pages[normalized] = (normalized === normalizeURL(baseURL)) ? 0 : 1;
+
+    try {
+        const response = await fetch(currentURL);
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const htmlBody = await response.text();
+        const currentURLs = getURLSFromHTML(htmlBody, rootURL);
+        //console.log(currentURL);
+
+        // Process each URL found on the page
+        const crawlPromises = currentURLs.map(url => crawlPage(rootURL, url, pages));
+        // Wait for all recursive crawl operations to complete
+        await Promise.all(crawlPromises);
+    } catch (error) {
+        console.error('Fetch error:', error);
+    }
+    
+    return pages;
+}
+
 module.exports = {
     normalizeURL,
-    getURLSFromHTML
+    getURLSFromHTML,
+    crawlPage
 }
--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-console.log('hello')
+const { argv } = require('node:process')
+const { crawlPage } = require('./crawl.js')
+
+
+function main(){
+    //console.log(argv)
+    if (argv.length < 3){
+        console.error('No argument for the website to crawl')
+        return
+    }
+    if (argv.length > 3){
+        console.error('Too many arguments. Only one is allowed is pass.')
+        return
+    }
+    baseURL = argv[2]
+    console.log(`Starting crawler... at: ${baseURL}`)
+    crawlPage(baseURL, baseURL)
+        .then(pages => {
+            console.log(pages);
+        })
+}
+
+
+main()
